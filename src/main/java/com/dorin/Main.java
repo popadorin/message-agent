e7db717be8d4973641aaa37273fcf3dc8ddb2f5b
dorin95dp <dorin.popa1995@gmail.com>
2017-09-12 23:21:43 +0300

    Create initial application

---
+++ src/main/java/com/dorin/Main.java
@@ -0,0 +1,11 @@
+package com.dorin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // create Sender
+        // create Receiver
+        // create MessageBroker
+        System.out.println("Hello Vasea");
+    }
+}
